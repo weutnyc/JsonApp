4395bdc5aea9483fd671daf01d64ded4a660c9fb
weutnyc <weutnyc@gmail.com>
2021-05-03 09:06:48 +0300

    ready

---
+++ JsonApp/Dog.swift
@@ -0,0 +1,11 @@
+//
+//  Dog.swift
+//  JsonApp
+//
+//  Created by Anton on 30.04.2021.
+//
+
+struct Dog: Decodable {
+    let message: String?
+    let status: String
+}
---
+++ JsonApp/ImageViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ImageViewController.swift
+//  JsonApp
+//
+//  Created by Anton on 30.04.2021.
+//
+
+import UIKit
+
+class ImageViewController: UIViewController {
+
+    @IBOutlet var activityIndicator: UIActivityIndicatorView!
+    @IBOutlet var ImageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        activityIndicator.startAnimating()
+        activityIndicator.hidesWhenStopped = true
+        fetchImage()
+    
+    }
+    
+    private func fetchImage() {
+        guard let url = URL(string: "https://dog.ceo/api/breeds/image/random") else { return }
+        
+        URLSession.shared.dataTask(with: url) { (data, _, error) in
+            guard let data = data else {
+                print(error?.localizedDescription ?? "No error description")
+                return
+            }
+            
+            do {
+                let dog = try JSONDecoder().decode(Dog.self, from: data)
+                print(dog)
+                guard let imageURL = URL(string: dog.message ?? "") else { return }
+                guard let image = try? Data(contentsOf: imageURL) else { return }
+                
+                DispatchQueue.main.async {
+                    self.ImageView.image = UIImage(data: image)
+                    self.activityIndicator.stopAnimating()
+                }
+                
+            } catch let error {
+                print(error.localizedDescription)
+            }
+        }.resume()
+    }
+}
